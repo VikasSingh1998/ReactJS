eabd60dc15e0afdba513e097841784d4e67d68ed
vikas <svikaskumar21@gmail.com>
2024-08-27 23:44:50 +0530

    first commit for reactjs

---
+++ 11_Basic/ex1.jsx
@@ -0,0 +1,16 @@
+/*
+React ==> JS Library.
+React is widely used to create Single Page Applications (SPAs).
+A React Single Page Application (SPA) is a type of web application built using React, where the entire app is served as a single HTML page. 
+Instead of loading new pages from the server, SPAs dynamically update the content on the current page using JavaScript, making the app feel faster and more responsive.
+=============================================
+Why React is Fast??
+==> React uses virtual DOM.
+==> Virtual DOM update only reqired data which we have to update.
+=============================================
+Why we should Learn?
+--> Maintained by facebook.
+--> High Demand due to fast speed.
+--> Mobile app development with React-Native. 
+=============================================
+*/
---
+++ 11_Basic/ex2.jsx
@@ -0,0 +1,14 @@
+/*
+How to run react js without installation
+=========================================
+React JS generally used with NPM.
+But we can also use with CDN.(Content delivery Network)
+------------------------------------------
+React js Environment setup 
+--------------------------
+Intall Node and NPM.
+A React Single Page Application (SPA) is a type of web application built using React, where the entire app is served as a single HTML page. Instead of loading new pages from the server, SPAs dynamically update the content on the current page using JavaScript, making the app feel faster and more responsive.
+---------------------------
+Any alternative to NPM??
+==> 
+*/
